13a4d0c9846febaacfaed46286a45f23b1e43d1a
Subhadip Das <subhadipdas911@gmail.com>
2025-02-09 12:03:09 +0530

    Created a copy of the original object

--- DesignPattern/src/main/java/PrototypeDesignPattern/Client.java
+++ DesignPattern/src/main/java/PrototypeDesignPattern/Client.java
@@ -3,8 +3,12 @@ package PrototypeDesignPattern;
 public class Client {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-
+		
+		Student s1 = new Student("Jhilik", 29, 85.76);
+		Student s2 = new Student(s1);
+		
+		System.out.println("Student s1: "+s1);
+		System.out.println("Student s2: "+s2);
 	}
 
 }
---
+++ DesignPattern/src/main/java/PrototypeDesignPattern/Student.java
@@ -0,0 +1,20 @@
+package PrototypeDesignPattern;
+
+public class Student {
+	
+	String name;
+	int age;
+	double psp;
+	
+	public Student(String name, int age, double psp) {
+		this.name = name;
+		this.age = age;
+		this.psp = psp;
+	}
+	
+	public Student(Student st) {
+		this.name = st.name;
+		this.age = st.age;
+		this.psp = st.psp;
+	}
+}
