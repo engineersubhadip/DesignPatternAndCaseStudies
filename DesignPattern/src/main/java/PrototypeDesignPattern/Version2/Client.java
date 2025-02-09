045b6a360a8f2eae12a02ff797a345fd70132308
Subhadip Das <subhadipdas911@gmail.com>
2025-02-09 13:13:24 +0530

    Using Copy Method we are avoiding OCP Violation

--- DesignPattern/src/main/java/PrototypeDesignPattern/Version2/Client.java
+++ DesignPattern/src/main/java/PrototypeDesignPattern/Version2/Client.java
@@ -15,6 +15,12 @@ public class Client {
 		System.out.println("St1: "+st1);
 		System.out.println("St1 Copy: "+st1Copy);
 		
+//		The disadvantage of this approach is that we have to implement copy() method to all the Child Class.
+//		For ex: If we do not implement copy method inside IntelStudent then
+//		Student st = new IntelStudent();
+//		Student stCopy = st.copy(); -> This will invoke the copy() of the Student Class.
+//		Whereas we want to provide a copy for the IntelStudent Class.
+		
 	}
 
 }
